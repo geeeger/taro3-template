5896ca993c3bed09a90c777574080157ab3fc0e8
geeeger <lph1988520@sina.com>
2021-01-16 11:54:20 +0800

    feat: update to 3.0.24

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,5 @@
 module.exports = {
   'extends': [
     'taro/react'
-  ],
-  "rules": {
-      "no-unused-vars": [
-          "error",
-          {
-              "varsIgnorePattern": "Taro"
-          }
-      ],
-      "react/jsx-filename-extension": [
-          1,
-          {
-              "extensions": [
-                  ".js",
-                  ".jsx",
-                  ".tsx"
-              ]
-          }
-      ]
-  },
-  "parser": "@typescript-eslint/parser",
-  "plugins": [
-      "@typescript-eslint"
-  ],
-  "parserOptions": {
-      "ecmaFeatures": {
-          "jsx": true
-      },
-      "useJSXTextNode": true,
-      "project": "./tsconfig.json"
-  }
+  ]
 }
